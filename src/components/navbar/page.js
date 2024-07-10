3d5e050c11c45dde9d11fcc5062d31acaa4cde63
MAN MOHIT <97469161+GM-897@users.noreply.github.com>
2024-07-11 01:00:14 +0530

    Update navbar/page.js

--- src/components/navbar/page.js
+++ src/components/navbar/page.js
@@ -1,13 +1,35 @@
 "use client"
 
 import Link from 'next/link';
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
+import { UserAuth } from '@/app/context/AuthContext';
+import { useRouter } from 'next/navigation';
 
 const Navbar = () => {
+  const {user, logOut} = UserAuth();
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    try {
+      await logOut();
+      router.push('/');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const checkAuthentication = async () => {
+      await new Promise((resolve) => setTimeout(resolve, 50));
+      setLoading(false);
+    };
+    checkAuthentication();
+  }, [user]);
 
 
   return (
-    <nav className="fixed flex align-middle top-0 backdrop-blur-md text-base border-b-[0.5px] border-b-slate-700 flex-row text-white py-4 px-6 md:px-10 w-3/4 justify-between" >
+    <nav className=" flex align-middle top-0 backdrop-blur-md text-base border-b-[0.5px] border-b-slate-700 flex-row text-white py-4 px-6 md:px-10 w-3/4 justify-between" >
           <div className="flex flex-row justify-center text-center align-middle">
             <Link href="/" className='flex flex-row items-center'>
               Space-Con
@@ -28,9 +50,27 @@ const Navbar = () => {
             </Link>
           </div>
           <div className="flex flex-row">
-
-            <button className="mx-2">Log In</button>
-            <button className="mx-2">Sign Up</button>
+          {loading ? null : !user ? (
+        <ul className="flex">
+          <li className="p-2 cursor-pointer">
+            <Link href = "/login" >
+            Login
+            </Link>
+          </li>
+          <li className="p-2 cursor-pointer">
+            <Link href ="/signup">
+            Sign up
+            </Link>
+          </li>
+        </ul>
+      ) : (
+        <div>
+          <p>Welcome, {user.displayName}</p>
+          <p className="cursor-pointer" onClick={handleSignOut}>
+            Sign out
+          </p>
+        </div>
+      )}
 
           </div>
         
